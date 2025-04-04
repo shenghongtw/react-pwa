9b28028f4d7540df0bf214384766da585301a990
shenghong <roryhaung1@gmail.com>
2025-04-05 01:26:17 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,42 @@ import path from 'path';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
-import manifest from './manifest.json';
+// import manifest from './manifest.json';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      manifest,
+      manifest: {
+        name: "React PWA",
+        short_name: "reactpwa",
+        description: "Starter kit for modern web applications",
+        theme_color: "#ffffff",
+        background_color: "#ffffff",
+        display: "standalone",
+        orientation: "portrait",
+        scope: "/",
+        start_url: "/?source=pwa",
+        icons: [
+          {
+            src: "pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          },
+          {
+            src: "pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable"
+          }
+        ]
+      },
       includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
       // 在开发环境中启用 PWA 进行测试
       devOptions: { 
